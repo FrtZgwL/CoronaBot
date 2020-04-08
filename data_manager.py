49eeac95b6deaa5576d631e5dd8695384dc4ffaf
Cedric <cedric.tonyedjon@gmail.com>
2020-04-08 11:29:52 +0200

    Set up data_manager.py structure

---
+++ data_manager.py
@@ -0,0 +1,37 @@
+
+
+# Check out "file_structure.md" to see where to get the data.
+
+def update_data():
+    """Pulls up to date data into the "data/"-folder."""
+    print("WIP")
+
+def per_population(**kwargs):
+    """Returns a saved image that plots numbers about the corona crisis from different countries relative to their population.
+
+    Keyword arguments: 
+    category -- "dead", "cured", "infected" or "active".
+    countries -- List with country names. For example ["Germany", "Italy", "USA"]
+    """
+    print("WIP")
+
+def since_outbreak(**kwargs):
+    """Returns a saved image that plots numbers about the corona crisis from different countries per day since the day they reached 100 cases.
+
+    Keyword arguments: 
+    category -- "dead", "cured", "infected" or "active".
+    countries -- List with country names. For example ["Germany", "Italy", "USA"]
+    """
+    print("WIP")
+
+def compare_deaths(**kwargs):
+    """Returns a list of saved images that show a cake-diagram that compares corona deaths with all other deaths. 
+
+    We assume that an average amount of people died since the first corona cases. (to compute this see "data/death_counts.csv") We assume that all people that are listed under corona-deaths would not have died otherwise. So we conclude that the total amount of deaths since the first corona cases is the average amount of deaths + the deaths from corona. This function shows how much of the total amount of deaths since the first corona cases are deaths from corona.
+
+    Keyword arguments:
+    countries -- List with country names. For example ["Germany", "Italy", "USA"]
+    """
+    print("WIP")
+
+
---
+++ file_structure.md
@@ -0,0 +1,6 @@
+
+# File Structure #
+
+- Day to day corona data is stored in "data/" as ".csv"-files with the name of the date. For example "data/04-07-2020.csv" Remember to run data.manager.update_data() to pull fresh data from the Internet.
+- Population sizes are stored in "data/populations.csv".
+- Death counts are stored in "data/death_counts.csv".
